6d94c4f361c9e0aa66efea55fa7d993b7daa037e
Himanshu Sharma <hhimanshusharma70@gmail.com>
2021-02-08 12:20:03 +0530

    added:

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
   return (
     <div className='App'>
       <div className='container'>
-        <h1>Test 32</h1>
+        <h1>Test 3211</h1>
       </div>
       <div className='repo-container'>
         <ListLoading isLoading={appState.loading} repos={appState.repos} />
